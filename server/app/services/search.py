75abc19dd5db49d36efa4ec92412dfd98fc1e778
wararaki <ky7.ott.w@gmail.com>
2020-09-24 01:54:51 +0900

    add comment

--- server/app/services/search.py
+++ server/app/services/search.py
@@ -9,6 +9,7 @@ from .response import ResponseFormatter
 class SearchService:
     @classmethod
     def search(clf, query: Optional[str], es: Elasticsearch, index: str) -> Tuple[int, List[News]]:
+        # TODO: 依存を取り除く。今は検索時に elasticsearch のメソッドを直接呼び出しているので、Elasticsearch から切り替えるとき、メソッドの改修コストが高くなる。
         es_query = QueryBuilder.build(query)
         response = es.search(index=index, body=es_query)
         return ResponseFormatter.format(response)
