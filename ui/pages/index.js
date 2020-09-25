7e8975c8894fec09dc1524c656821e0bff357706
wararaki <ky7.ott.w@gmail.com>
2020-09-26 00:59:55 +0900

    add page jump

--- ui/pages/index.js
+++ ui/pages/index.js
@@ -1,16 +1,36 @@
 import React from 'react';
 
 class Home extends React.Component {
-    render() {
-        return (
-        <div>
-          <form>
-            <input type="text" value='' />
-          </form>
-          <button>Search</button>
-        </div>
-        )
-    }
+  constructor (props) {
+    super(props);
+    this.state = {query: ''};
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  handleChange (event) {
+    this.setState({query: event.target.value});
+  }
+  
+  search() {
+    const self = this;
+    const params = new URLSearchParams({q: self.state.query});
+    const url = `http://localhost:3000/search?${params}`;
+    
+    location.href = url;
+  }
+
+  render() {
+      return (
+      <div>
+        <form>
+          <input type="text" value={this.state.query} onChange={this.handleChange} />
+        </form>
+        <button onClick={this.search}>Search</button>
+      </div>
+      )
+  }
 }
 
 export default Home;
