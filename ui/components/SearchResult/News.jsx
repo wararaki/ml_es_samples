f6ec87c7114564a503bb4e177c32532a749bdde8
wararaki <ky7.ott.w@gmail.com>
2020-10-15 03:16:32 +0900

    fix

--- ui/components/SearchResult/News.jsx
+++ ui/components/SearchResult/News.jsx
@@ -1,6 +1,6 @@
 const News = (props) => {
   return (
-    <div className="News" >
+    <div className="News">
       <h3>{props.data.title}</h3>
       <p>{props.data.datetime}</p>
       <p>{props.data.content}</p>
