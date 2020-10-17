547c6078bb017f6e3134ac1ae0978ceba0f99e0d
wararaki <ky7.ott.w@gmail.com>
2020-10-18 00:55:44 +0900

    fix directory structure

--- ui/src/pages/_app.js
+++ ui/src/pages/_app.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 
-import theme from '../src/theme';
+import theme from '../styles/theme';
 
 
 const MyApp = (props) => {
