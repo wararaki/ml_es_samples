6b124289234fbbd0dcbd9f35f2635bb4f108612f
wararaki <ky7.ott.w@gmail.com>
2020-10-22 00:12:38 +0900

    fix search styles

--- ui/src/components/SearchResult/News.jsx
+++ ui/src/components/SearchResult/News.jsx
@@ -1,10 +1,25 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+const useStyles = makeStyles({
+  news: {
+    margin: '10px 0'
+  }
+});
+
+
 const News = (props) => {
+  const classes = useStyles();
+
   return (
-    <div className="News">
-      <h3>{props.data.title}</h3>
-      <p>{props.data.datetime}</p>
-      <p>{props.data.content}</p>
-    </div>
+    <Card className={ classes.news }>
+      <CardContent>
+        <h3>{props.data.title}</h3>
+        <p>{props.data.datetime}</p>
+        <p>{props.data.content}</p>
+      </CardContent>
+    </Card>
   );
 }
 
