16e34557e8c45b34d18df073d0635261cd347e55
wararaki <ky7.ott.w@gmail.com>
2020-10-22 00:35:14 +0900

    fix form design

--- ui/src/components/SearchForm/SearchForm.jsx
+++ ui/src/components/SearchForm/SearchForm.jsx
@@ -1,9 +1,31 @@
 import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1
+  },
+  iconButton: {
+    padding: 10
+  }
+}));
 
 
 const SearchForm = (props) => {
+  const classes = useStyles();
+
   const [query, setQuery] = useState(props.query);
 
   const handleChange = (event) => {
@@ -22,12 +44,17 @@ const SearchForm = (props) => {
   };
 
   return (
-    <div>
-      <form onSubmit={ onFormSubmit }>
-        <TextField label="Search..." variant="outlined" value={ query } onChange={ handleChange } />
-      </form>
-      <Button variant="outlined" onClick={ search }>Search</Button>
-    </div>
+    <Paper component="form" className={classes.root} onSubmit={ onFormSubmit }>
+      <InputBase
+        className={ classes.input }
+        placeholder="Search..."
+        value={ query }
+        onChange={ handleChange }
+      />
+      <IconButton type="submit" className={ classes.iconButton } aria-label="search" onClick={ search }>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
   );
 }
 
--- ui/src/pages/_document.js
+++ ui/src/pages/_document.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { ServerStyleSheet as StyledComponentSheets } from 'styled-components';
-import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/styles';
+import { ServerStyleSheets } from '@material-ui/styles';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 
 import theme from '../theme';
@@ -24,25 +23,19 @@ export default class MyDocument extends NextDocument {
 
 
 MyDocument.getInitialProps = async (ctx) => {
-  const styledComponentSheets = new StyledComponentSheets();
-  const materialUiSheets = new MaterialUiServerStyleSheets();
+  const sheets = new ServerStyleSheets();
   const originalRenderPage = ctx.renderPage;
   
-  try {
-    ctx.renderPage = () => originalRenderPage({
-      enhanceApp: (App) => (props) => styledComponentSheets.collectStyles(materialUiSheets.collect(<App { ...props } />))
-    });
-    
-    const initialProps = await NextDocument.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: [
-        ...React.Children.toArray(initialProps.styles),
-        materialUiSheets.getStyleElement(),
-        styledComponentSheets.getStyleElement()
-      ]
-    };
-  } finally {
-      styledComponentSheets.seal();
-  }
+  ctx.renderPage = () => originalRenderPage({
+    enhanceApp: (App) => (props) => sheets.collect(<App { ...props } />)
+  });
+
+  const initialProps = await NextDocument.getInitialProps(ctx);
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement()
+    ]
+  };
 };
